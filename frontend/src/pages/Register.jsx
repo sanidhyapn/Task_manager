aeb96f955b5db9f90bc685bc7d4446dca3797338
sanidhyapn <pnsanidhya@gmail.com>
2025-03-31 15:53:37 +0530

    updated Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://task-manager-backend-9hv8.onrender.com/api/auth/register", {
         name,
         email,
         password,
